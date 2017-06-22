3c4e1f07770c246fa00381943024f70c627ddc14
Marcus Cobden <marcus@weave.works>
2017-06-22 10:09:53 +0100

    First work towards an app version

---
+++ src/datasource/module.ts
@@ -0,0 +1,10 @@
+import Datasource from "./datasource";
+
+class AnnotationsQueryCtrl {
+  static templateUrl = 'partials/annotations.editor.html'
+}
+
+class ConfigCtrl {
+}
+
+export {AnnotationsQueryCtrl, ConfigCtrl, Datasource};
--- src/module.ts
+++ src/module.ts
@@ -6,6 +6,29 @@ class AnnotationsQueryCtrl {
 
 class ConfigCtrl {
   static templateUrl = 'partials/config.html';
+  private enabled: boolean;
+  private $q: any;
+  private appEditCtrl: any;
+  private appModel any;
+
+  constructor($scope: any, $injector: any, $q: any) {
+    this.$q = $q;
+    this.enabled = false;
+    this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+  }
+
+  postUpdate() {
+    if (!this.appModel.enabled) {
+      return this.$q.resolve();
+    }
+    return this.appEditCtrl.importDashboards().then(() => {
+      this.enabled = true;
+      return {
+        url: "plugins/raintank-kubernetes-app/page/clusters",
+        message: "Kubernetes App enabled!"
+      };
+    });
+  }
 }
 
 export {AnnotationsQueryCtrl, ConfigCtrl, Datasource};
